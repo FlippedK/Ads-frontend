a9e6ea9904cc889d18e89a7f98a90867c04d9def
FlippedK <72911923+FlippedK@users.noreply.github.com>
2021-03-13 10:03:23 +0500

    Add files via upload

---
+++ localhost/api.php
@@ -0,0 +1,17 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=ads_db','root','root');
+
+if (isset($_GET['add'])) {
+    include_once "api_add.php";
+}
+
+else if (isset($_GET['all'])) {
+    
+    $query = $pdo -> query("SELECT * FROM ads");
+    $query = $query -> fetchAll(PDO::FETCH_ASSOC);
+
+    header("Content-type: application/json; charset=utf-8");
+
+    echo json_encode($query);
+}
